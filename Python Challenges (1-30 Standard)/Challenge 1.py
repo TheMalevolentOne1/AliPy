ab4a4421ab9101c4db3bc1c8036387f88813e370
TheMalevolentOne1 <kylerobinson818@gmail.com>
2024-07-18 23:10:39 +0100

    upload

---
+++ Python Challenges (1-30 Standard)/Challenge 1.py
@@ -0,0 +1,11 @@
+'''
+Challenge 1
+Write a program that:
+• asks the user to input their age
+• outputs 'Your age is: ' followed by their age.
+'''
+
+
+age = int(input("What is your age?:\n"))
+
+print(f"Your age is: {age}")
---
+++ Python Challenges (1-30 Standard)/Challenge 2.py
@@ -0,0 +1,15 @@
+'''
+Challenge 2
+Write a program that:
+• asks the user to input two numbers
+• calculates the average
+• outputs the result
+
+You will need to use:
+Sequencing
+Variables
+
+Tip: To calculate the average of two numbers you need
+to add them together and divide them by two.
+'''
+
